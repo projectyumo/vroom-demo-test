81c1d4748ce3690b3e45a7c05218ced646b311c6
Kevin Payumo <projectyumo@gmail.com>
2025-01-21 20:35:29 -0800

    reqs update

--- main.py
+++ main.py
@@ -23,9 +23,21 @@ SHOPIFY_API_SECRET = os.environ.get("SHOPIFY_API_SECRET")
 APP_URL = os.environ.get("APP_URL")
 FIREBASE_ID = os.environ.get("FIREBASE_ID")
 FIREBASE_URL = os.environ.get("FIREBASE_URL")
-FIREBASE_CREDENTIALS = os.environ.get("FIREBASE_CREDENTIALS")
+FIREBASE_CREDENTIALS = {
+                          "type": os.environ.get("FIREBASE_TYPE"),
+                          "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+                          "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+                          "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
+                          "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+                          "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+                          "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+                          "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+                          "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER"),
+                          "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509"),
+                          "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+                        }
 
-cred = credentials.Certificate(json.loads(FIREBASE_CREDENTIALS))
+cred = credentials.Certificate(FIREBASE_CREDENTIALS)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
